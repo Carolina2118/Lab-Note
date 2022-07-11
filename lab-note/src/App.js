1b6661292b29ed0627f9ef8f3e9d09244bead8d7
Carolina2118 <loboscarolinaa@gmail.com>
2022-07-11 19:15:06 -0400

    metodo de ingreso de firebase Auth

--- lab-note/src/App.js
+++ lab-note/src/App.js
@@ -1,18 +1,27 @@
 import React from 'react'
+import { useRef } from 'react';
 import { registro } from "./firebase"
 
 
-
+// el registro sera async porque el registro sera devolvera una promesa 
 export default function App() {
-  function resolverRegistro(){
-    
+   
+  const emailRef = useRef();
+  const passwordRef = useRef();
+
+
+  async function resolverRegistro(){
+
+// asignamos campos de entrada a la referencia 
+//emailRef.current.value optengo el valor actual de la referencia de entrada del correo y contraseña
+await registro(emailRef.current.value, passwordRef.current.value);
   }
   return (
     <div id="main">
 
       <div id="campos">
-        <input placeholder='Email' />
-        <input type="password" placeholder="Password"/>
+        <input ref={emailRef} placeholder='Email' />
+        <input ref={passwordRef} type="password" placeholder="Password"/>
       </div>
 
       <button onClick={resolverRegistro}>Enviar</button>
